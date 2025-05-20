8a239b291b750e2c58cef2c3df628e68fd52a861
unknown <oghazaly96@gmail.com>
2025-05-20 18:45:11 +0300

    Reverted back to i18n.ts approach

--- frontend/next-i18next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'de', 'fr', 'es', 'ar'], // Add all supported locales here
-    },
-    localeDetection: true,
-}; 
